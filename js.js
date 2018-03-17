4062ec1d9378e5de013a8c76f5dcfdbfa14abcf5
moueza <mouezapeter@gmail.com>
2018-03-17 10:14:58 +0100

    Update js.js
    https://www.w3schools.com/xml/ajax_xmlhttprequest_create.asp

--- js.js
+++ js.js
@@ -2,14 +2,32 @@
 //https://www.w3schools.com/js/js_function_definition.asp
 function () {
  function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  // var xmlhttp = new XMLHttpRequest();
+   var xmlhttp;
+  
+  if (window.XMLHttpRequest) {
+   
+    xmlhttp = new XMLHttpRequest();
+ } else {
+    // code for old IE browsers
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+  
+  
+ 
+  xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      document.getElementById("demo").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "ajax_info.txt", true);
-  xhttp.send();
+  xmlhttp.open("GET", "ajax_info.txt", true);
+  xmlhttp.send();
+  
+  
+  
+  
+  
+  
 }
     
     
